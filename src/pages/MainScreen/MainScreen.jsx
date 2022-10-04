5655adc521b94834c367f4071f5285e5cb7b7a98
Thiago 02 <thiago.ralves02@gmail.com>
2022-10-04 18:47:09 -0300

    feat: mainscreen page

--- src/pages/MainScreen/MainScreen.jsx
+++ src/pages/MainScreen/MainScreen.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Actions } from '../../actions/Actions'
+import Header from '../../components/Header/Header'
+import InfoContainer from '../../components/InfoContainer/InfoContainer'
+import Post from '../../components/Post/Post'
+import { MainScreenContainer, MainScreenContent } from './style'
 
 export default function MainScreen() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        getAllPosts()
+    }, [])
+
+    const getAllPosts = async () => {
+        const getAll = await Actions.getAll()
+        setPosts(getAll)
+    }
+
     return (
-        <div>MainScreen</div>
+        <MainScreenContainer>
+            <MainScreenContent>
+                <Header
+                    className="main"
+                    title="CodeLeap Network"
+                />
+                <InfoContainer
+                    className="main"
+                    title="What's on your mind?"
+                    inputLabel="Title"
+                    inputName="postTitle"
+                    inputPlaceholder="Hello World"
+                    textareaLabel="Content"
+                    textareaName="postContent"
+                    textareaPlaceholder="Content here"
+                    buttonLabel="CREATE"
+                    onClickBtn=""
+                />
+                {posts?.map((post, index) =>
+                    <Post
+                        key={index}
+                        post={post}
+                    />
+                )}
+            </MainScreenContent>
+        </MainScreenContainer>
     )
 }
---
+++ src/pages/MainScreen/style.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import { media } from "../../assets/style/media"
+
+export const MainScreenContainer = styled.main`
+    display: flex;
+    justify-content: center;
+`
+
+export const MainScreenContent = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 60%;
+    background-color: #ffff;
+
+    ${media('tablet')}{
+        width:80%;
+    }
+
+    ${media('mobile')}{
+        width:100%;
+    }
+`
